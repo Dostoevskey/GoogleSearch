caf52cac40a5680296ae98c467d911f5be9906cf
Igor <dostoevskey@gmail.com>
2018-09-26 04:40:57 -0400

    Added 'WebdriverWait' method and fixed locator.

--- src/test/java/testPackage/SearchGoogle.java
+++ src/test/java/testPackage/SearchGoogle.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -14,31 +16,37 @@ public class SearchGoogle {
     protected WebDriver driver;
 
     @BeforeTest
-    public final void setupTest() {
+    public void setupTest() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://www.google.com/ncr");
     }
 
     @Test
-    public final void doFindSelenium() {
+    public void doFindSelenium() {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
         driver.findElement(By.name("q")).sendKeys("selenium" + "\n");
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id='ires' and contains(.,'Selenium - Web Browser Automation')]")).
-                getText().contains("Selenium - Web Browser Automation"));
+        Assert.assertEquals(driver.findElement(By.xpath("//h3[contains(.,'Selenium - Web Browser Automation')]")).
+                getText(), "Selenium - Web Browser Automation");
+
         driver.findElement(By.cssSelector("#hdtb-msb-vis div:nth-child(3) a")).click();
+        driver.findElement(By.xpath("//img[@id='1kNJvnyWka1zxM:']")).isDisplayed();
         driver.findElement(By.xpath("//img[@id='1kNJvnyWka1zxM:']")).click();
-        driver.findElement(By.xpath("//img[@id='1kNJvnyWka1zxM:']")).isDisplayed(); //Sometimes failed.
-        Assert.assertTrue(driver.findElement(By.xpath("//a[contains(.,'SeleniumHQ')]")).
-                getText().contains("SeleniumHQ"), ("Object is not matching, "));
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath
+                ("//span[@class='irc_su' and contains(.,'GitHub - SeleniumHQ/selenium: A browser automation framework and ecosystem.')]")));
+        Assert.assertEquals(driver.findElement(By.xpath
+                ("//span[@class='irc_su' and contains(.,'GitHub - SeleniumHQ/selenium: A browser automation framework and ecosystem.')]")).
+                getText(), "GitHub - SeleniumHQ/selenium: A browser automation framework and ecosystem.");
+
         driver.findElement(By.cssSelector("#hdtb-msb-vis div:nth-child(1) a")).isEnabled();
         driver.findElement(By.cssSelector("#hdtb-msb-vis div:nth-child(1) a")).click();
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id='ires' and contains(.,'Selenium - Web Browser Automation')]")).
-                getText().contains("Selenium - Web Browser Automation"));
-
+        Assert.assertEquals(driver.findElement(By.xpath("//h3[contains(.,'Selenium - Web Browser Automation')]")).
+                getText(), "Selenium - Web Browser Automation");
     }
 
     @AfterTest
-    public final void teardown() {
+    public void teardown() {
         if (driver != null) {
             driver.close();
         }
